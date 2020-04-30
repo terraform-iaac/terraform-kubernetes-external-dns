038fe3217bf81a0a1ef41be97e0bacff96917ff0
bohdantverdyi <ownedd777@gmail.com>
2020-04-30 19:00:47 +0300

    GSIAAC-26 Add type for variables

--- variables.tf
+++ variables.tf
@@ -2,13 +2,16 @@ locals {
   name = "external-dns"
 }
 variable "dns" {
+  type = string
   description = "(Required) DNS name: exmaple.com"
 }
 variable "service_type_ip" {
+  type = string
   description = "(Optional) Which ips resolves for services. public or private"
   default = "private"
 }
 variable "txt_owner_id" {
+  type = string
   description = "(Optional) TXT Owner"
   default = "external-dns"
 }
@@ -17,6 +20,7 @@ variable "namespace_labels" {
   default = {}
 }
 variable "whitelist_annotation" {
+  type = string
   description = "(Optional) Annotations in yaml for external dns. External dns updates resource with this annotation"
   default = "external-dns=use"
 }
